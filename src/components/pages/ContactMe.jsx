336b2f11819ede9a96fb794bd70eed159b2e2793
Flagg217 <flagg217@gmail.com>
2023-10-26 18:04:42 -0700

    worked on ContatMe file

--- src/components/pages/ContactMe.jsx
+++ src/components/pages/ContactMe.jsx
@@ -1,14 +1,80 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import Header from "../Header";
+import Footer from "../Footer";
 
 
-const Contact = ()=>{
+function ContactMe() {
 
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-    const [subject, setSubject] = useState('')
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+        if (inputType === 'name') {
+            setName(inputValue);
+        } else if (inputType === 'email') {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !email || !message) {
+            setErrorMessage('Please fill out all fields');
+            return;
+        }
+        alert(`Hello ${name}`);
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+    const fontColor = "black";
+    return (
+        <div>
+            <Header title="Contact Me" bgColor="lightblue" />
+            <div style={{ color: fontColor }}>
+                <form className="form">
+                    <input
+                        value={name}
+                        name="name"
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="Name"
+                    />{" "}
+                    <br />
+                    <input
+                        value={email}
+                        name="email"
+                        onChange={handleInputChange}
+                        type="email"
+                        placeholder="Email"
+                    />
+                    <br />
+                    <input
+                        value={message}
+                        name="message"
+                        onChange={handleInputChange}
+                        type="text"
+                        placeholder="Message"
+                    />
+                    <br />
+                    <button type="button" onClick={handleFormSubmit}>Submit</button>
+                </form>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+            </div>
+            <Footer title="Contact Me" bgColor="lightblue" />
+        </div>
+    );
 }
 
-export default Contact
+export default ContactMe;
